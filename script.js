00bedb294b3d10050a0ac31372df972e0bb12298
freddycarta <cartafederico92@gmail.com>
2022-10-10 14:49:17 +0200

    js correttp

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ console.log('js ok')
 // stampo in consolle da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
-
 const numero = i + 1
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 if( numero % 3 === 0 && numero % 5 === 0 ){
