28732ebdb67698c47e25488aef6259542c3e6293
freddycarta <cartafederico92@gmail.com>
2022-10-10 14:34:09 +0200

    2 commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('js ok')
+
+// stampo in consolle da 1 a 100
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+
+
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// se num è multiplo di 3 stampo fizz, se invece multiplo di 5 stampo buzz.
+// sennò numero normale i + 1
+
+
+console.log(i + 1)
+
+}
+
