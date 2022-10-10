51554bfb0500714e13539aded811a0bd82b77fc8
freddycarta <cartafederico92@gmail.com>
2022-10-10 14:45:14 +0200

    js

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ console.log('js ok')
 for (let i = 1; i <= 100; i++) {
     console.log(i)
 
-
+const numero = i + 1
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-// se num è multiplo di 3 stampo fizz, se invece multiplo di 5 stampo buzz.
-// sennò numero normale i + 1
-
-
-console.log(i + 1)
+if( numero % 3 === 0 && numero % 5 === 0 ){
+    console.log('Fizzbuzz')
+} else if( numero % 3 === 0 ) {
+    // se num è multiplo di 3 stampo fizz
+    console.log('Fizz')
+} else if( numero % 5 === 0 ) {
+    // se num è multiplo di 5 stampo buzz
+    console.log('Buzz')
+} else {
+    // sennò numero normale i + 1
+    console.log(numero)}
 
 }
 
